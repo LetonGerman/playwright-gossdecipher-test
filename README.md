e06764a4c23824600a6fa077ab88a1ad8c66b889
german <neveraskedfor@gmail.com>
2021-11-13 08:20:35 +0300

    added support for env variables

---
+++ README.md
@@ -0,0 +1,3 @@
+# Playwright based testing for https://kodaktor.ru/g/_72ef40a/eb498
+
+## Needs an .env file in root folder with listed variables: TEST_HOST (decipher server), KEY_FILE, SECRET_FILE, EXPECTED (expected output value)
--- e2e/decipher.spec.js
+++ e2e/decipher.spec.js
@@ -1,14 +1,20 @@
-const { test, expect } = require('@playwright/test');
-const striptags = require('striptags');
+const { test, expect } = require("@playwright/test");
+const striptags = require("striptags");
 
-test('decipher test', async ({ page }) => {
-  await page.goto('https://kodaktor.ru/g/_72ef40a/eb498');
+test("decipher test", async ({ page }) => {
 
-  const elementHandle = await page.$('form');
-  await elementHandle.evaluate(node => node.setAttribute('action', "https://decrypt-app.herokuapp.com"));
-  await page.setInputFiles('input[name=key]', 'id_rsa2');
-  await page.setInputFiles('input[name=secret]', 'secret2');
-  await page.locator('[type=submit]').click();
+  if ([process.env.TEST_HOST, process.env.KEY_FILE, process.env.SECRET_FILE, process.env.EXPECTED].some(el => el === undefined)) {
+    console.error("Not all the required environment variables were defined. Please set up an .env file with listed variables: TEST_HOST (decipher server), KEY_FILE, SECRET_FILE, EXPECTED (expected output value)");
+    throw("Not all the required environment variables were defined");
+  }
+  await page.goto("https://kodaktor.ru/g/_72ef40a/eb498");
+
+  const elementHandle = await page.$("form");
+  const host = process.env.TEST_HOST;
+  await elementHandle.evaluate((node, [host]) => node.setAttribute("action", host), [host]);
+  await page.setInputFiles("input[name=key]", process.env.KEY_FILE);
+  await page.setInputFiles("input[name=secret]", process.env.SECRET_FILE);
+  await page.locator("[type=submit]").click();
 
   await page.waitForSelector("iframe");
   const responseHandle = await page.$("[name=ifr]");
@@ -18,5 +24,5 @@ test('decipher test', async ({ page }) => {
   const frameInnerContent = await frame.$eval("body", node => node.innerHTML);
   const cleanFrameInnerContent = striptags(frameInnerContent); 
   console.log(cleanFrameInnerContent);
-  expect(cleanFrameInnerContent.trim()).toBe("matrix");
+  expect(cleanFrameInnerContent.trim()).toBe(process.env.EXPECTED);
 });
